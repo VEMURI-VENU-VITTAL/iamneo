5f02ec121a01e46e8e9466d1959869363456ddb7
unknown <venuvittalvemuri@gmail.com>
2022-10-14 17:18:46 +0530

    ready

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const ejsMate=require("ejs-mate");
 const path=require('path');
 const methodOverride=require("method-override");
 const mongoose=require("mongoose");
+const axios=require('axios')
 const User=require('./models/user');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
@@ -107,6 +108,31 @@ app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash('error');
     next();
+})
+
+const dataapi=async function(){
+  const res=await axios.get(`https://randomuser.me/api/?results=5000`);
+  const {results}=res.data;
+  for(let i of results){
+    const gender=i.gender;
+    const name=i.name.first+" "+i.name.last;
+    const address=i.location.number+i.location.name;
+    const country=i.location.country;
+    const email=i.email;
+    const password=i.login.password;
+    const dob=i.dob.date.slice(0,10);
+    const number=i.phone;
+    const imageurl=i.picture.large;
+    const imagefolder='Random/fv4a2gcykdt622pewpwc';
+    const user=await new User({author:"634599a84879320187e09728",name:name,email:email,dob:dob, address:address,number:number, password:password,country:country,gender:gender,image:[{url:imageurl,filename:imagefolder}]})
+    await user.save();
+
+  }
+
+}
+
+app.get('/insert',(req,res)=>{
+dataapi();
 })
 
 //'mongodb://localhost:27017/taskTwo'
